c44343bbfee8802fd8215bd6b75aa0d02b0d36a6
Rajan <krajan92946@gmail.com>
2024-05-02 14:57:06 +0530

    Rajan

--- bitwise/DecToBinary.cpp
+++ bitwise/DecToBinary.cpp
@@ -22,26 +22,4 @@
 
 // }
 
-#include<iostream>
-#include<math.h>
-using namespace std;
-int main()
-{
-  int n;
-  int bit;
-  int ans=0;
-  int i=0;
-  cout<<"Enter the decimal number:";
-  cin>>n;
-  while(n!=0)
-  {
-    bit=n%10;
-    if(bit==1)
-    {
-      ans=bit*pow(2,i)+ans;
-    }
-    n=n/10;
-    i++;
-  }
-  cout<<ans;
-}
+
